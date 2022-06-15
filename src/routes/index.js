702e556b5a168aa0c49d2615bcebfc57e6fb5c70
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-15 18:55:13 -0300

    docs: setup branch Likes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import timelineRouter from "./timelineRouter.js";
 import hashtagRouter from "./hashtagRouter.js";
 import autheticationRouter from "./authenticationRouter.js";
+import likeRouter from "./likeRouter.js";
 
 const router = Router();
 
 router.use(hashtagRouter);
 router.use(timelineRouter);
 router.use(autheticationRouter);
+router.use(likeRouter);
 
 export default router;
---
+++ src/routes/likeRouter.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const likeRouter = Router();
+
+likeRouter.post();
+likeRouter.post();
+
+export default likeRouter;
