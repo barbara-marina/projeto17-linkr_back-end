75a7fa23e356b5947f7d1d19ef4013d87baddc20
thyagolucas <salc@icomp.ufam.edu.br>
2022-06-16 02:09:30 -0400

    feat: login concluído

--- src/repositories/infoUsers.js
+++ src/repositories/infoUsers.js
@@ -1,37 +1,69 @@
 import db from "../../config/db.js";
 
-export async function getUserByEmail(email){
+export async function isThereEmail(email){
+ 
+    const user = await db.query(`
+                            SELECT * FROM users
+                            WHERE email = $1
+                        `,[email]);
 
-    return db.query(`
-        SELECT * FROM users 
-        WHERE email = $1
-    `,[email]);
+    return user.rowCount === 0 
+    ? false : true ;
+}
 
+export async function createUser(email, passCrypt, username, picture_url){
+    
+    const create = await db.query(`INSERT 
+                                    INTO users (email, password, username, picture)
+                                    VALUES ($1, $2, $3, $4)`, 
+                                    [email, passCrypt, username, picture_url]);
+
+    return create.rowCount === 0
+    ? false : true ; 
+}
+
+export async function login(email){
+    try {
+        const user = await db.query(`
+            SELECT  u.id, u.email, u.password
+            FROM users u 
+            WHERE email = $1
+                    
+        `,[email]);
+
+    return user.rows[0];
+        
+    } catch (error) {
+    console.log('PER37: Falha ao criar logar')
+    }
 }
 
 export async function createSession(userId, token){
     try {
         await db.query(`
-                INSERT INTO sessions ('userId', token) 
+                INSERT INTO sessions ("userId", token) 
                 VALUES ($1, $2)         
     `,[userId, `${token}`]);
-
-    return 0;
+    
+    return 0;    
         
     } catch (error) {
-    console.log('Falha ao criar session')
+    console.log('Falha ao criar session',error)
     }
 }
 
-export async function userLogout(email){
+export async function checkToken(token){
 
-    return db.query(`
-        UPDATE sessions
-        SET valid = false
-        WHERE token = $1 AND valid = $2
-    `,[token, true]);
+    const isThere =  await db.query(`
+                                    SELECT s.valid,
+                                    FROM sessions s
+                                    JOIN users u 
+                                    WHERE token = $1
+                                    `,[token]);
 
-}
+    return isThere.rows[0]
+
+}//não terminado
 
 export async function getUserByToken(token){
 
@@ -43,23 +75,15 @@ export async function getUserByToken(token){
 
     return valid ? userId : valid;
 
-}
-export async function getUserByToken(email){
-
-    return db.query(`
-        SELECT * FROM users 
-        WHERE email = $1
-    `,[email]);
-
-}
-
+}//não terminado
 
 
 const infoUsers = {
-    getUserByEmail,
-    getUserByToken,
+    isThereEmail,
+    createUser,
+    login,
     createSession,
-    userLogout
+ 
 }
 
 export default infoUsers
--- src/routes/authenticationRouter.js
+++ src/routes/authenticationRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createUser, login, logout } from "../controllers/authenticationController.js";
 import { schemasValidations } from "../middlewares/schemasMiddleware.js";
-import vDatasToRegister from "../schemas/userSchema.js";
+import schemaDataToRegister from "../schemas/schemaDataToRegister.js";
+import schemaIsLogin from "../schemas/schemaIsLogin.js";
 
 const autheticationRouter = Router();
 
-autheticationRouter.post("/sign-up", schemasValidations(vDatasToRegister),  createUser);
-autheticationRouter.post("/sign-in", login);
+autheticationRouter.post("/sign-up", schemasValidations(schemaDataToRegister),  createUser);
+autheticationRouter.post("/sign-in", schemasValidations(schemaIsLogin), login);
 autheticationRouter.put("/logout", logout)
 
 export default autheticationRouter;
