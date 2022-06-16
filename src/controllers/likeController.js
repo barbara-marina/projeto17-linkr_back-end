fe8c89c43c6bb15169c190e5483444ed219a26b7
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-16 18:22:59 -0300

    feat: delegando consultas no banco de dados

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,32 +1,23 @@
-import db from "../../config/db";
+import likeRepository from "../repositories/likeRepository.js";
+import timelineRepository from "../repositories/timelineRepository.js";
 
 export async function createLike(req, res){
     const {id} = req.params;
     const user = req.locals.user;
     
     try {
-        console.log('teste request');
         //TODO: validar se o post existe e está publicado
-        const post = await db.query(`
-            SELECT * FROM "posts" WHERE id = $1
-        `, [Number(id)]);
+        const post = await timelineRepository.getPostById(parseInt(id));
         const [postId] = post.rows;
-
-        const verifyPost = !postId || postId.deleted || post.rowCount !== 1;
+        const verifyPost = !postId || postId.deleted || post.rowCount !== 1 || postId.id !== Number(id);
         if(verifyPost) return res.sendStatus(401);
 
         //TODO: validar se o usuario ja deu like -> não deve permitir
-        const like = await db.query(`
-            SELECT * FROM "likes" WHERE userId = $1 AND postId = $2
-        `, [user.id, Number(id)]);
-        
+        const like = await likeRepository.getLikeUserPost(user.id, parseInt(id));
         const [likeId] = like.rows;
         if(likeId) return res.sendStatus(401);
 
-        await db.query(`
-            INSERT INTO "likes" ("userId", "postId", "createdAt")
-            VALUES ($1, $2, NOW())
-        `, [user.id, Number(id)]);
+        await likeRepository.insertLikeUserPost(user.id, parseInt(id));
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
@@ -39,27 +30,18 @@ export async function createDislike(req, res){
     const user = req.locals.user;
     
     try {
-        console.log('teste request');
         //TODO: validar se o post existe e está publicado
-        const post = await db.query(`
-            SELECT * FROM "posts" WHERE id = $1
-        `, [Number(id)]);
+        const post = await timelineRepository.getPostById(parseInt(id));
         const [postId] = post.rows;
-
         const verifyPost = !postId || postId.deleted || post.rowCount !== 1;
         if(verifyPost) return res.sendStatus(401);
 
         //TODO: validar se o usuario ja deu like -> deve permitir
-        const allowDislike = await db.query(`
-            SELECT * FROM "likes" WHERE userId = $1 AND postId = $2
-        `, [user.id, Number(id)]);
-        
+        const allowDislike = await likeRepository.getLikeUserPost(user.id, parseInt(id));    
         const [dislikeId] = allowDislike.rows;
         if(!dislikeId) return res.sendStatus(401);
         
-        await db.query(`
-            DELETE FROM "likes" WHERE "userId" = $1 AND "postId" = $2
-        `, [user.id, Number(id)]);
+        await likeRepository.deleteLikeUserPost(user.id, parseInt(id));
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
