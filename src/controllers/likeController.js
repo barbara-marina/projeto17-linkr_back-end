4b0eb871521f6710b8ccae323cbc2bfbe7aeed85
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-16 18:33:54 -0300

    feat: update likeRepository

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -6,16 +6,17 @@ export async function createLike(req, res){
     const user = req.locals.user;
     
     try {
-        //TODO: validar se o post existe e está publicado
         const post = await timelineRepository.getPostById(parseInt(id));
+        
         const [postId] = post.rows;
         const verifyPost = !postId || postId.deleted || post.rowCount !== 1 || postId.id !== Number(id);
         if(verifyPost) return res.sendStatus(401);
 
-        //TODO: validar se o usuario ja deu like -> não deve permitir
         const like = await likeRepository.getLikeUserPost(user.id, parseInt(id));
+        
         const [likeId] = like.rows;
-        if(likeId) return res.sendStatus(401);
+        const verifyLike = likeId && like.rowCount === 1;
+        if(verifyLike) return res.sendStatus(401);
 
         await likeRepository.insertLikeUserPost(user.id, parseInt(id));
         res.sendStatus(200);
@@ -30,16 +31,17 @@ export async function createDislike(req, res){
     const user = req.locals.user;
     
     try {
-        //TODO: validar se o post existe e está publicado
         const post = await timelineRepository.getPostById(parseInt(id));
+        
         const [postId] = post.rows;
-        const verifyPost = !postId || postId.deleted || post.rowCount !== 1;
+        const verifyPost = !postId || postId.deleted || post.rowCount !== 1 || postId.id !== Number(id);
         if(verifyPost) return res.sendStatus(401);
 
-        //TODO: validar se o usuario ja deu like -> deve permitir
         const allowDislike = await likeRepository.getLikeUserPost(user.id, parseInt(id));    
+        
         const [dislikeId] = allowDislike.rows;
-        if(!dislikeId) return res.sendStatus(401);
+        const verifyDislike = !dislikeId || allowDislike.rowCount !== 1;
+        if(verifyDislike) return res.sendStatus(401);
         
         await likeRepository.deleteLikeUserPost(user.id, parseInt(id));
         res.sendStatus(200);
