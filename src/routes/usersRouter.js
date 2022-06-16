6da07fb7ad6d4ed7284436fe876bf58ff1e133c2
barbarina <barbaramarinaufsm@gmail.com>
2022-06-16 15:30:31 -0300

    feat: add get user related info
    get user posts by user id and get users list by username part

---
+++ src/routes/usersRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import usersController from "../controllers/usersController.js";
+import { validationToken } from "../middlewares/tokenMiddleware.js";
+
+const usersRouter = Router();
+
+usersRouter.get("/user/:id", validationToken, usersController.getPostsByUserId);
+usersRouter.get("/user/list/:username", validationToken, usersController.listUsers);
+
+export default usersRouter;
