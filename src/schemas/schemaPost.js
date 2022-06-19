85559ade81fff29a8c1edb2d363a8d76fdf4b622
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-06-19 11:28:03 -0300

    schema post

--- src/schemas/schemaPost.js
+++ src/schemas/schemaPost.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const schemaPost = joi.object({
     url: joi.string().uri().required(),
-    description: joi.string()
+    description: joi.string().allow('')
 });
 
 export default schemaPost;
