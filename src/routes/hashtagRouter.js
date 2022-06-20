271744d08a08be2019462432a530d10b93cbdfee
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-06-20 07:35:16 -0300

    Update hashtagRouter.js

--- src/routes/hashtagRouter.js
+++ src/routes/hashtagRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { getHashtagPosts, getHashtags } from "../controllers/hashtagController.js";
+import { validationToken } from "../middlewares/tokenMiddleware.js";
 
 const hashtagRouter = Router();
 
 hashtagRouter.get("/hashtag", getHashtags);
-hashtagRouter.get("/hashtag/:hashtag", getHashtagPosts);
+hashtagRouter.get("/hashtag/:hashtag", validationToken, getHashtagPosts);
 
 export default hashtagRouter;
