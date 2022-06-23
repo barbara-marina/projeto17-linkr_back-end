23a04614b060c79f37da8c23339746cc6b39d151
barbarina <barbaramarinaufsm@gmail.com>
2022-06-23 05:38:53 -0300

    fix: get user posts by id

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,20 +3,34 @@ import usersRepository from "../repositories/usersRepository.js";
 
 async function getPostsByUserId(req, res) {
     const { id } = req.params;
+    const userId = res.locals.user.id;
 
     try {
-        const userData = await usersRepository.getPostsByUserId(parseInt(id));
-        const likeUser = await timelineRepository.likesUsersPost();
-
+        const userData = await usersRepository.getUserData(parseInt(id));
+        
         if (userData.rowCount === 0) return res.status(404).send("Usuário não existe.");
+        
+        const result = await usersRepository.getPostsByUserId(parseInt(id));
+        const likeUser = await timelineRepository.likesUsersPost();
+        const posts = [];
 
-        if (userData.rows[0].userPosts[0]===null) {
-            userData.rows[0].userPosts.shift();
+        for(let post of result.rows){
+
+            const iLiked = likeUser.rows.some( element => element.userId == parseInt(userId) && element.postId == post.id )
+            const whoLiked = likeUser.rows.filter(element => element.postId == post.id);
+            const thePost = {iLiked:iLiked, whoLiked:whoLiked, post:post}
+            posts.push(thePost);
+
         }
+
+        console.log({
+            userData: userData.rows[0],
+            posts: posts
+        });
         
         res.send({
-            posts: userData.rows[0],
-            usersLikes: likeUser.rows
+            userData: userData.rows[0],
+            posts: posts
         });
     } catch(error) {
         console.log(error);
