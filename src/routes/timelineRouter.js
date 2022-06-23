0f9e84aaab177dd38738591c5b1ef1acfe001768
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-06-23 20:35:46 -0300

    feat: test query

--- src/routes/timelineRouter.js
+++ src/routes/timelineRouter.js
@@ -16,3 +16,31 @@ timelineRouter.get('/timeline/open/:id', getPostRedirect);
 timelineRouter.get('/timelineall', getAllPosts);
 
 export default timelineRouter;
+
+/* 
+-- Na página de timeline (rota /timeline), agora devem aparecer somente os posts das pessoas que você segue e não mais todo mundo
+
+#1
+    SELECT p.*, u.id AS "userId", u.username,
+    COUNT(l."postId") AS "likes", tst.id, tst."userId", tst.following
+    FROM "posts" p
+    LEFT JOIN "likes" l ON l."postId" = p."id"
+    JOIN (
+        select * from "followers" where "userId" = 2
+    ) AS tst
+        ON tst."following" = p."userId"
+    JOIN "users" u ON p."userId" = u."id"
+    WHERE p."deleted" = false
+    GROUP BY p."id", u."id", tst."id", tst."userId", tst.following
+    ORDER BY p."createdAt"
+    
+#2
+    SELECT s."repostUserId", p.* 
+    FROM shared s
+    JOIN posts p ON s."postId" = p.id
+    JOIN (
+        SELECT * FROM "followers" f WHERE f."userId" = 4
+         ) AS teste
+    ON teste."following" = s."repostUserId"
+    WHERE p."deleted" = false;
+*/
