10f660c3fe85e1b79dfb433c47dab462c0d02fb1
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-06-24 14:33:22 -0400

    fix: priorize on search the users that i follow

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,6 +5,6 @@ import { validationToken } from "../middlewares/tokenMiddleware.js";
 const usersRouter = Router();
 
 usersRouter.get("/user/:id", validationToken, usersController.getPostsByUserId);
-usersRouter.get("/user/list/:username", validationToken, usersController.listUsers);
+usersRouter.get("/user/list/:userId/:username", validationToken, usersController.listUsers);
 
 export default usersRouter;
